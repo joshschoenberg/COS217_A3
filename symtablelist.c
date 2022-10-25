0e7ff400587243143e1a003474ebf1c500d6b16e
Josh <jschoenberg@princeton.edu>
2022-10-24 20:10:19 -0400

    third draft of new, free, and getlength

--- symtablelist.c
+++ symtablelist.c
@@ -45,3 +45,53 @@ size_t SymTable_getLength(SymTable_T oSymTable) {
     return oSymTable->symTableSize;
 }
 
+ int SymTable_put(SymTable_T oSymTable, const char *pcKey, 
+                                                  const void *pvValue) {
+    assert(oSymTable != NULL);
+    if (oSymTable->first == NULL) {
+        oSymTable->first = calloc(1, sizeof(*pcKey));
+
+    }
+    /* Go through oSymTable to check if pcKey already exists */
+
+    /* If it does, return 0. */
+
+    /* If it does not, add it in and return 1. */
+    
+    /* If insufficient mememory, return 0. */
+
+}
+
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey,
+    const void *pvValue) {
+    assert(oSymTable != NULL);
+
+    }
+
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey) {
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
+
+}
+
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey) {
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
+
+}
+
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey){
+    assert(oSymTable != NULL);
+    assert(pcKey != NULL);
+
+}
+
+void SymTable_map(SymTable_T oSymTable,
+    void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+    const void *pvExtra) {
+    
+    assert(oSymTable != NULL);
+    assert(pvExtra != NULL);
+    
+
+    }
