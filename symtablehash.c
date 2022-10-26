c6774bf56b3c9f3b62eb0b2f7ae0d2d19860f0e6
Josh <jschoenberg@princeton.edu>
2022-10-25 21:14:45 -0400

    hash new draft 1

--- symtablehash.c
+++ symtablehash.c
@@ -7,16 +7,56 @@
 #include <assert.h>
 #include "symtable.h"
 
+enum { BUCKET_COUNT = 509 };
+
+struct Binding {
+    const char *pcKey; 
+    void *pvValue; 
+    struct Binding *next;
+    };
+
+struct HashTable {
+    struct Binding *buckets[BUCKET_COUNT];
+};
+
+struct SymTable {
+    struct HashTable *psFirst;
+    size_t bindingsCount;
+    size_t uBucketCount;
+    };
+
+/* Return a hash code for pcKey that is between 0 and uBucketCount-1,
+   inclusive. */
+
+static size_t SymTable_hash(const char *pcKey, size_t uBucketCount)
+{
+   const size_t HASH_MULTIPLIER = 65599;
+   size_t u;
+   size_t uHash = 0;
+
+   assert(pcKey != NULL);
+
+   for (u = 0; pcKey[u] != '\0'; u++)
+      uHash = uHash * HASH_MULTIPLIER + (size_t)pcKey[u];
+
+   return uHash % uBucketCount;
+}
+
 SymTable_T SymTable_new(void) {
-    struct Node {
-        const char *key;
-        int value;
-        struct Node *next;
-    };
-    struct SymTable_T {
-        struct Node *first;
-    };
-    return ;
+    SymTable_T oSymTable;
+    oSymTable = (SymTable_T) calloc(1, sizeof(struct SymTable));
+    if(oSymTable == NULL) {
+        return NULL;
+        }
+    oSymTable->psFirst = (struct HashTable *) calloc(1, sizeof(struct HashTable));
+    if (oSymTable->psFirst == NULL) {
+        free(oSymTable);
+        return NULL;
+    }
+    oSymTable->bindingsCount = 0;
+    oSymTable->uBucketCount = BUCKET_COUNT;
+    
+    return oSymTable;
 }
 
 void SymTable_free(SymTable_T oSymTable) {
